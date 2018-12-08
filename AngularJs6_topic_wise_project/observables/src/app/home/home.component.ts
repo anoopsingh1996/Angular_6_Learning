36ccba5b94bd41bd277afdd6d46f38db3449905c
anoopsingh1996 <anoopraghuvansi@gmail.com>
2018-12-08 13:53:33 +0530

    unsubscribe obserable

--- AngularJs6_topic_wise_project/observables/src/app/home/home.component.ts
+++ AngularJs6_topic_wise_project/observables/src/app/home/home.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import {Observer} from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    // const myNumbers = Observable.interval(1000);
-    // myNumbers.subscribe(
-    //   (number: number) => {
-    //      console.log(number);
-    //   }
-    // );
+    const myNumbers = Observable.interval(1000);
+  this.numbersObsSubscription =   myNumbers.subscribe(
+      (number: number) => {
+         console.log(number);
+      }
+    );
     const myObservable = Observable.create((observer: Observer<string>) => {
           setTimeout(() => {
             observer.next('first package');
@@ -34,11 +38,16 @@ export class HomeComponent implements OnInit {
             observer.next('third package');
           },6000);
     });
-    myObservable.subscribe(
+  this.customObsSubscription  =  myObservable.subscribe(
       (data:string) => { console.log(data); },
       (error: string) => { console.log(error);},
       () => { console.log('completed');},
     );
+  }
+
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 
 }
