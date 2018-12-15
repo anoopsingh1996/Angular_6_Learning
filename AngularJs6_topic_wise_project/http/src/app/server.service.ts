3fb53944c1b25bad7417e6d0b949227eb64dc6d2
anoopsingh1996 <anoopraghuvansi@gmail.com>
2018-12-15 22:04:24 +0530

    http in angular

---
+++ AngularJs6_topic_wise_project/http/src/app/server.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+
+@Injectable()
+export class ServerService {
+  constructor(private http: Http) {}
+  storeServers(servers: any[]) {
+    const headers = new Headers({'Content-Type':'application/json'})
+    return this.http.post('https://first-1502.firebaseio.com/data.json',
+    servers,
+    {headers: headers});
+  }
+  getServers() {
+    return this.http.get('https://first-1502.firebaseio.com/data.json');
+  }
+}
