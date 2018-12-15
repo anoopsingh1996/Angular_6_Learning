bb152cce8b030c6f1f06fabb002532a6022608b4
anoopsingh1996 <anoopraghuvansi@gmail.com>
2018-12-16 00:23:36 +0530

    POST, GET, PUT in http

--- AngularJs6_topic_wise_project/http/src/app/server.service.ts
+++ AngularJs6_topic_wise_project/http/src/app/server.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs'
 
 
 @Injectable()
@@ -7,11 +9,28 @@ export class ServerService {
   constructor(private http: Http) {}
   storeServers(servers: any[]) {
     const headers = new Headers({'Content-Type':'application/json'})
-    return this.http.post('https://first-1502.firebaseio.com/data.json',
+    // return this.http.post('https://first-1502.firebaseio.com/data.json',
+    // servers,
+    // {headers: headers});
+    return this.http.put('https://first-1502.firebaseio.com/data.json',
     servers,
     {headers: headers});
   }
   getServers() {
-    return this.http.get('https://first-1502.firebaseio.com/data.json');
+    return this.http.get('https://first-1502.firebaseio.com/data.json')
+    .map(
+      (response: Response) => {
+        const data = response.json();
+        for (const server of data) {
+          server.name = 'FETCHED_' + server.name;
+        }
+        return data;
+      }
+    )
+    .catch(
+      (error: Response) => {
+        return Observable.throw('some things went wrong');
+      }
+    );
   }
 }
